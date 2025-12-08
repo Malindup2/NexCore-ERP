1f80bcc7be0dee29fd76066dc55df6fabccfb893
malindup2 <malindupabasara22@gmail.com>
2025-12-08 22:06:00 +0530

    feat: add Payroll module with database  migrations

---
+++ backend/services/PayrollService/Migrations/20251208163450_InitialCreate.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace PayrollService.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SalaryRecords",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    EmployeeId = table.Column<int>(type: "integer", nullable: false),
+                    EmployeeName = table.Column<string>(type: "text", nullable: false),
+                    Department = table.Column<string>(type: "text", nullable: false),
+                    BasicSalary = table.Column<decimal>(type: "numeric(18,2)", nullable: false),
+                    Allowances = table.Column<decimal>(type: "numeric(18,2)", nullable: false),
+                    NetSalary = table.Column<decimal>(type: "numeric(18,2)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SalaryRecords", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SalaryRecords");
+        }
+    }
+}
---
+++ backend/services/PayrollService/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using PayrollService.Data;
+using Serilog;
+using Shared.Logging;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//Setup Logging
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilogLogging();
+
+// Setup Database
+builder.Services.AddDbContext<PayrollDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
